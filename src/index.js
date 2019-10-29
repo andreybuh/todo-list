73fbbb4caa8f7417e7123cf90c7f8b6f7ffd4113
andrey <andrey.buhantsov@esteit.com>
2019-10-29 15:10:57 +0300

    update

--- src/index.js
+++ src/index.js
@@ -6,47 +6,114 @@ import './index.css';
 import Button from '@material-ui/core/Button';
 
 
-class AppComponent extends React.Component {
-    state = {
-        numChildren: 0
-    };
-
-    render () {
-        const children = [];
-
-        for (var i = 0; i < this.state.numChildren; i += 1) {
-            children.push(<ChildComponent key={i} number={i} />);
-        }
-
-        return (
-            <ParentComponent addChild={this.onAddChild}>
-                {children}
-            </ParentComponent>
-        );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newItem: "",
+            listToDo: [],
+            listDone: []
+        };
     }
 
-    onAddChild = () => {
+    updateInput(key, value) {
+        // update react state
+        this.setState({[key]: value});
+    }
+
+    addItem() {
+        // create a new item with unique id
+        const newItem = {
+            id: 1 + Math.random(),
+            value: this.state.newItem.slice()
+
+        };
+
+        // copy current list of items
+        const listToDo = [...this.state.listToDo];
+
+        // add the new item to the list
+        listToDo.push(newItem);
+
+        // update state with new list, reset the new item input
         this.setState({
-            numChildren: this.state.numChildren + 1
+            listToDo,
+            newItem: ""
         });
+
+    }
+
+    deleteItem(id) {
+        // copy current list of items
+        const listToDo = [...this.state.listToDo];
+        // filter out the item being deleted item => item.id !== id
+        const updatedList = listToDo.filter(function (item) {
+            return item.id !== id
+        });
+
+
+        this.setState({listToDo: updatedList});
+    }
+
+    addDoneItem(id) {
+        // copy current list of items
+        const listToDo = [...this.state.listToDo];
+
+        const listNew = listToDo.splice(this.state.listToDo);
+
+        this.setState({listDone: listNew});
+
+        console.log(listNew, 'первый');
+        console.log(this.state.listDone);
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div>
+                        <br/>
+                        <input
+                            type="text"
+                            placeholder="Type item here"
+                            value={this.state.newItem}
+                            onChange={e => this.updateInput("newItem", e.target.value)}
+                        />
+                        <button onClick={() => this.addItem()} disabled={!this.state.newItem.length}>
+                            <i> + </i>
+                        </button>
+                        <br/> <br/>
+                        <ul>
+                            {this.state.listToDo.map(item => {
+                                return (
+                                    <li key={item.id}>
+                                        {item.value}
+                                        <button onClick={() => this.addDoneItem(item.id)}>
+                                            <i>x</i>
+                                        </button>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+
+                        <ul>
+                            {this.state.listDone.map(item => {
+                                return (
+                                    <li key={item.id}>
+                                        {item.value}
+                                        <button onClick={() => this.deleteItem(item.id)}>
+                                            <i>x</i>
+                                        </button>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
-const ParentComponent = props => (
-    <div className="card calculator">
-        <p><a href="#" onClick={props.addChild}>Add Another Child Component</a></p>
-        <div id="children-pane">
-            {props.children}
-        </div>
-    </div>
-);
 
-const ChildComponent = props => <div>{"I am child " + props.number}</div>;
-
-
-// ========================================
-
-ReactDOM.render(
-    <AppComponent/>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
